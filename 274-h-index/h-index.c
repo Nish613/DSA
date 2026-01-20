1ece0a636c2dfb9a0b4a0b834a0766f698a02a76
Nish613 <nishtha2711@gmail.com>
2026-01-21 00:03:39 +0530

    Time: 28 ms (13.38%) | Memory: 8.3 MB (51.41%) - LeetSync

---
+++ 274-h-index/h-index.c
@@ -0,0 +1,12 @@
+int hIndex(int* citations, int n) {
+    int best = 0;
+
+    for (int h = 1; h <= n; h++) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= h) count++;
+        }
+        if (count >= h) best = h;
+    }
+    return best;
+}
